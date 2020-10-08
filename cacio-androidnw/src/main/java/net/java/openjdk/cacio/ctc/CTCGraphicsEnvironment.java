7ef15e32c9e1054393243809bffb23d6530b2210
khanhduytran0 <khanhduytran032@gmail.com>
2020-10-08 09:52:52 +0700

    Move dummy font list to correct place

--- cacio-androidnw/src/main/java/net/java/openjdk/cacio/ctc/CTCGraphicsEnvironment.java
+++ cacio-androidnw/src/main/java/net/java/openjdk/cacio/ctc/CTCGraphicsEnvironment.java
@@ -7,42 +7,71 @@ import java.lang.reflect.*;
 import java.awt.*;
 import java.awt.image.*;
 
+import sun.font.FontConfigManager;
+import static sun.font.FontConfigManager.*;
+
 import sun.java2d.SunGraphicsEnvironment;
 import sun.java2d.SurfaceManagerFactory;
 
 public class CTCGraphicsEnvironment extends SunGraphicsEnvironment {
-	static {
-		android.os.OpenJDKNativeRegister.registerNatives();
-		
-		try {
-			/*
-			 * Make AWT use Caciocavallo and not load libawt_xawt.so
-			 * to prevent linking X11 libraries.
-			 */
-			 
-			// Initialize headless mode first
-			Class.forName("java.awt.Toolkit");
+    static {
+        android.os.OpenJDKNativeRegister.registerNatives();
 
-			// Set false it...
-			System.setProperty("java.awt.headless", "false");
-			
-			// Set false to GraphicsEvnironment saved isHeadless
-			Field headlessField = GraphicsEnvironment.class.getDeclaredField("headless");
-			headlessField.setAccessible(true);
-			headlessField.set(null, Boolean.FALSE);
-			
-			Field defaultHeadlessField = GraphicsEnvironment.class.getDeclaredField("defaultHeadless");
-			defaultHeadlessField.setAccessible(true);
-			defaultHeadlessField.set(null, Boolean.FALSE);
-			
-			// System.setProperty("awt.toolkit", "net.java.openjdk.cacio.ctc.CTCToolkit");
-			// System.setProperty("java.awt.graphicsenv", "net.java.openjdk.cacio.ctc.CTCGraphicsEnvironment");
-		} catch (Throwable th) {
-			System.err.println("Unable to unheadless:");
-			th.printStackTrace();
-		}
-	}
-	
+        // Android: add a dummy font list
+        FontConfigFont[] fcFont = new FontConfigFont[2];
+
+        fcFont[0] = new FontConfigFont();
+        fcFont[0].familyName = "Android Roboto Sans";
+        fcFont[0].styleStr = "Regular";
+        fcFont[0].fullName = "Android Roboto Sans Regular";
+        fcFont[0].fontFile = "/system/fonts/DroidSans.ttf";
+
+        fcFont[1] = new FontConfigFont();
+        fcFont[1].familyName = "Android Roboto Sans";
+        fcFont[1].styleStr = "Bold";
+        fcFont[1].fullName = "Android Roboto Sans Bold";
+        fcFont[1].fontFile = "/system/fonts/DroidSans-Bold.ttf";
+
+        FcCompFont font = new FcCompFont();
+        font.fcName = "sans"; //monospace
+        font.fcFamily = "sans"; // eg sans
+        font.jdkName = "sansserif"; // eg sansserif
+        font.style = 0; // eg 0=PLAIN
+        font.allFonts = fcFont;
+        font.firstFont = fcFont[0];
+        // font.compFont = ...; 
+        FontConfigManager.FcCompFont[] fontArr = new FcCompFont[]{font};
+        FontConfigManager.populateFontConfig(fontArr);
+        
+        try {
+            /*
+             * Make AWT use Caciocavallo and not load libawt_xawt.so
+             * to prevent linking X11 libraries.
+             */
+            /*
+             // Initialize headless mode first
+             Class.forName("java.awt.Toolkit");
+
+             // Set false it...
+             System.setProperty("java.awt.headless", "false");
+
+             // Set false to GraphicsEvnironment saved isHeadless
+             Field headlessField = GraphicsEnvironment.class.getDeclaredField("headless");
+             headlessField.setAccessible(true);
+             headlessField.set(null, Boolean.FALSE);
+
+             Field defaultHeadlessField = GraphicsEnvironment.class.getDeclaredField("defaultHeadless");
+             defaultHeadlessField.setAccessible(true);
+             defaultHeadlessField.set(null, Boolean.FALSE);
+             */
+            // System.setProperty("awt.toolkit", "net.java.openjdk.cacio.ctc.CTCToolkit");
+            // System.setProperty("java.awt.graphicsenv", "net.java.openjdk.cacio.ctc.CTCGraphicsEnvironment");
+        } catch (Throwable th) {
+            System.err.println("Unable to unheadless:");
+            th.printStackTrace();
+        }
+    }
+
     public CTCGraphicsEnvironment() {
         SurfaceManagerFactory.setInstance(new CTCSurfaceManagerFactory());
     }
@@ -51,7 +80,7 @@ public class CTCGraphicsEnvironment extends SunGraphicsEnvironment {
     protected int getNumScreens() {
         return 1;
     }
-	
+
     @Override
     protected GraphicsDevice makeScreenDevice(int screennum) {
         return new CTCGraphicsDevice();
@@ -61,17 +90,17 @@ public class CTCGraphicsEnvironment extends SunGraphicsEnvironment {
     public boolean isDisplayLocal() {
         return true;
     }
-	
-	// Headless override
-/*
-	@Override
-	public boolean isHeadlessInstance() {
-        return false;
-    }
-	
-	@Override
-	public static boolean isHeadless() {
-		return false;
-	}
-*/
+
+    // Headless override
+    /*
+     @Override
+     public boolean isHeadlessInstance() {
+     return false;
+     }
+
+     @Override
+     public static boolean isHeadless() {
+     return false;
+     }
+     */
 }
--- cacio-tta/src/main/java/net/java/openjdk/cacio/ctc/CTCGraphicsEnvironment.java
+++ cacio-tta/src/main/java/net/java/openjdk/cacio/ctc/CTCGraphicsEnvironment.java
@@ -2,41 +2,10 @@ package net.java.openjdk.cacio.ctc;
 
 import java.awt.GraphicsDevice;
 
-import sun.font.FontConfigManager;
-import static sun.font.FontConfigManager.*;
-
 import sun.java2d.SunGraphicsEnvironment;
 import sun.java2d.SurfaceManagerFactory;
 
 public class CTCGraphicsEnvironment extends SunGraphicsEnvironment {
-    static {
-        // Android: add a dummy font
-        FontConfigFont[] fcFont = new FontConfigFont[2];
-        
-        fcFont[0] = new FontConfigFont();
-        fcFont[0].familyName = "Android Roboto Sans";
-        fcFont[0].styleStr = "Regular";
-        fcFont[0].fullName = "Android Roboto Sans Regular";
-        fcFont[0].fontFile = "/system/fonts/DroidSans.ttf";
-
-        fcFont[1] = new FontConfigFont();
-        fcFont[1].familyName = "Android Roboto Sans";
-        fcFont[1].styleStr = "Bold";
-        fcFont[1].fullName = "Android Roboto Sans Bold";
-        fcFont[1].fontFile = "/system/fonts/DroidSans-Bold.ttf";
-        
-        FcCompFont font = new FcCompFont();
-        font.fcName = "sans"; //monospace
-        font.fcFamily = "sans"; // eg sans
-        font.jdkName = "sansserif"; // eg sansserif
-        font.style = 0; // eg 0=PLAIN
-        font.allFonts = fcFont;
-        font.firstFont = fcFont[0];
-        // font.compFont = ...; 
-        FontConfigManager.FcCompFont[] fontArr = new FcCompFont[]{font};
-        FontConfigManager.populateFontConfig(fontArr);
-    }
-    
     public CTCGraphicsEnvironment() {
         SurfaceManagerFactory.setInstance(new CTCSurfaceManagerFactory());
     }
