fa0dc619ea56c513e518197c2b16237022c62147
ArtDev <45949002+artdeell@users.noreply.github.com>
2021-02-03 00:22:03 +0300

    Update CTCScreen.java

--- cacio-androidnw/src/main/java/net/java/openjdk/cacio/ctc/CTCScreen.java
+++ cacio-androidnw/src/main/java/net/java/openjdk/cacio/ctc/CTCScreen.java
@@ -110,7 +110,7 @@ public class CTCScreen implements PlatformScreen {
             return /*instance.screenBuffer.getRGB(0, 0,
                 (int) FullScreenWindowFactory.getScreenDimension().getWidth(),
                 (int) FullScreenWindowFactory.getScreenDimension().getHeight(),
-                null, 0, (int) FullScreenWindowFactory.getScreenDimension().getWidth());*/((DataBufferInt)(instance.screenBuffer.getRaster().getDataBuffer())).getData();;
+                null, 0, (int) FullScreenWindowFactory.getScreenDimension().getWidth());*/((DataBufferInt)(instance.screenBuffer.getRaster().getDataBuffer())).getData();
         }
     }
     
