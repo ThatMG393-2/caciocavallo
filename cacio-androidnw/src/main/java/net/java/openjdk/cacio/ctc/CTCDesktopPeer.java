09e6b8d4a8015d61466bfbb6a98c799d2f47519b
ArtDev <45949002+artdeell@users.noreply.github.com>
2023-01-26 22:27:53 +0300

    Basic Desktop peer for testing

---
+++ cacio-androidnw/src/main/java/net/java/openjdk/cacio/ctc/CTCDesktopPeer.java
@@ -0,0 +1,34 @@
+package net.java.openjdk.cacio.ctc;
+
+import java.awt.peer.DesktopPeer;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.awt.Desktop.Action;
+
+public class CTCDesktopPeer implements DesktopPeer {
+   public boolean isSupported(Action action) {
+     switch(action) {
+       case OPEN:
+       case BROWSE:
+         return true;
+       default:
+         return false;
+     }
+   }
+  public void open(File file) throws IOException {
+    System.out.println("peer open: "+file.getAbsolutePath());
+  }
+  public void edit(File file) throws IOException {
+    throw new IOException("Action not supported");
+  }
+  public void print(File file) throws IOException {
+    throw new IOException("Action not supported");
+  }
+  public void mail(URI mailtoURL) throws IOException {
+    throw new IOException("Action not supported");
+  }
+  public void browse(URI uri) throws IOException {
+    System.out.println("peer browse: "+uri.toString());
+  }
+}
